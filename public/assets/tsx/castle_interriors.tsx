be8e17037c662e5c16d7cf9c6aa3c2d431e2c581
Moses Maximino Cosme II (mosesintech) <mosesmaxcosme@gmail.com>
2025-06-06 20:45:07 -0500

    initial commit

---
+++ public/assets/tsx/castle_interriors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="castle_interriors" tilewidth="32" tileheight="32" tilecount="1320" columns="33">
+ <image source="../tilesets/RPG_Buildings_CASTLE_v1.0/castle_interriors.png" width="1056" height="1280"/>
+</tileset>
